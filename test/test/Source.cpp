ab5cf0b012fdfd58e03c4e85ef77623be214c659
Alice (SandurskiyNB) <alice@example.com>
2023-07-05 00:19:41 +0300

    code: добавлен вывод разности

--- test/test/Source.cpp
+++ test/test/Source.cpp
@@ -5,6 +5,6 @@ int main() {
 	cout << "Enter A and B: ";
 	int a, b;
 	cin >> a >> b;
-	cout << "A + B = " << a + b;
-
+	cout << "A + B = " << a + b << endl;
+	cout << "A - B = " << a - b << endl;
 }
